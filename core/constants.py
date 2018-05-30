7930ffd4c71b844deda2bf7264533123a189008b
Aleksei Pankratov <ruckus@yandex-team.ru>
2018-05-30 16:20:27 +0300

    Baklejan botid and battletimes

--- core/constants.py
+++ core/constants.py
@@ -3,13 +3,10 @@ from datetime import time
 MINIMUM_SQUAD_MEMBER_LEVEL = 1
 DAYS_PROFILE_REMIND = 100
 DAYS_OLD_PROFILE_KICK = 150
-CWBOT_ID = 587303845
+CWBOT_ID = 265204902
 TRADEBOT_ID = 278525885
 BATTLE_TIMES = [
-    time(hour=0),
-    time(hour=4),
-    time(hour=8),
-    time(hour=12),
-    time(hour=16),
-    time(hour=20)
+    time(hour=1),
+    time(hour=9),
+    time(hour=17)
 ]
